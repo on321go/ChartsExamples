4fc3ea97f0a1bef23b3e524451ad1c3d7851e0be
Joey Rubin <132927010+on321go@users.noreply.github.com>
2024-03-15 14:59:38 -0700

    Initial Commit

---
+++ ChartsExamples/ChartsExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartsExamplesApp.swift
+//  ChartsExamples
+//
+//  Created by jrubin on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct ChartsExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
